7b2f6a63f752f6002ba1dfa47b208a57ad31ab5f
hung135 <hung135@hotmail.com>
2017-03-24 11:58:54 -0400

    initial

---
+++ functions/postgres/fn_util_list_get.sql
@@ -0,0 +1,62 @@
+﻿DELIMITER $$
+
+DROP FUNCTION IF EXISTS `fn_util_list_get` $$
+CREATE FUNCTION `fn_util_list_get`(vList longText charset utf8mb4,vDelimiter Text,vPosition Int) RETURNS longtext CHARSET utf8mb4
+    DETERMINISTIC
+BEGIN
+	DECLARE pos_count INT; 
+	DECLARE x1,x2,x3 INT; 
+	DECLARE listLen,delimLen  INT; 
+	SET x1 = 1;
+	SET x2 = 1;
+	SET x3 = 1;
+	SET pos_count = 1;
+	set delimLen=char_length(vDelimiter);
+
+	if vList is null then
+		return null;
+	else
+		set listLen=fn_util_list_len(vList,vDelimiter);
+		
+	
+		
+			if (vPosition>listLen) then
+				return null;
+			elseif (vPosition=0) then
+					return vList;
+			
+			elseif (vPosition=1) then
+					set x1=locate(vDelimiter,vList);
+					if (x1=0) then
+						return vList;
+					else 
+						return left(vList,locate(vDelimiter,vList)-1);
+					end if;
+					
+			else
+				while pos_count<=vPosition do
+							set x1=x2;
+							set x2=x3;
+							if pos_count=1 then
+								set x3=locate(vDelimiter,vList,x3);
+							else
+								set x3=locate(vDelimiter,vList,x3+delimLen);
+							end if;
+					set pos_count=pos_count+1;
+				end while;
+				-- return x2;
+			  -- return concat(x1,"<-x1-",x2,"<-x2-",x3,"<-x3- len->",listLen,"--start:",x2+delimLen);
+					
+					if x3=0 then
+						return substring(vList,x2+delimLen,char_length(vList)-x2+delimLen);
+					else
+						return substring(vList,x2+delimLen,x3-(x2+delimLen));
+					end if;
+			end if;
+		end if;
+	
+	
+
+END $$
+
+DELIMITER ;
---
+++ functions/postgres/fn_util_list_len.sql
@@ -0,0 +1,24 @@
+﻿DELIMITER $$
+/*
+Given a Text String and a delimiter string.
+This function will return the number of items in the list include blank positions
+*/
+DROP FUNCTION IF EXISTS `fn_util_list_len` $$
+CREATE FUNCTION `fn_util_list_len`(vList longText,vdelimiter varchar(16)) RETURNS int(11)
+    DETERMINISTIC
+BEGIN
+	DECLARE x INT; 
+	SET x = 0;
+
+	if vList is null then
+		return x;
+	else
+	
+	set x=(char_length(vList)-char_length(replace(vList,vdelimiter,'')))/char_length(vdelimiter);
+	return x+1;
+	end if;
+	
+
+END $$
+
+DELIMITER ;
